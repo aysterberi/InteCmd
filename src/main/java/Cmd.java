b39a5bade55695398e1954e027ad46ded421c45c
Joakim Berglund <joakimberglund@live.se>
2016-10-10 17:46:05 +0200

    refactor

--- src/main/java/Cmd.java
+++ src/main/java/Cmd.java
@@ -12,7 +12,26 @@ public class Cmd {
         return line.split(" ");
     }
 
-    public Scanner getScanner() {
-        return scanner;
+    public void processInput() {
+        while (true) {
+            System.out.print(">> ");
+            String[] tokens = this.tokenizeString(scanner.nextLine());
+            switch (tokens[0]) {
+                case "exit":
+                    System.exit(1);
+                case "echo":
+                    for (int i = 1; i < tokens.length; i++)
+                        System.out.print(tokens[i] + " ");
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Unrecognized command");
+                    break;
+            }
+        }
+    }
+
+    public void processInput(String mockInput) {
+
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,22 +3,6 @@ public class Runner {
 
     public static void main(String[] args) {
         Cmd cmd = new Cmd();
-
-        while (true) {
-            System.out.print(">> ");
-            String[] str = cmd.tokenizeString(cmd.getScanner().nextLine());
-            switch (str[0]) {
-                case "exit":
-                    System.exit(1);
-                case "echo":
-                    for (int i = 1; i < str.length; i++)
-                        System.out.print(str[i] + " ");
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("Unrecognized command");
-                    break;
-            }
-        }
+        cmd.processInput();
     }
 }
