483a0f6d4809e7ffbae291e468824890964c4d0f
Joakim Berglund <joakimberglund@live.se>
2016-10-11 10:04:34 +0200

    refactor and add new (non-functioning) tests

--- src/main/java/Cmd.java
+++ src/main/java/Cmd.java
@@ -9,6 +9,19 @@ public class Cmd {
     }
 
     public String[] tokenizeString(String line) {
+        if (line.contains("'")) {
+            String[] arr = line.split("'");
+            String[] tokens = new String[line.split("'").length];
+            String quote = "";
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].split(" ").length > 2) {
+                    quote += "'" + arr[i] + "'";
+                    tokens[i] = quote;
+                } else
+                    tokens[i] = arr[i].trim();
+            }
+            return tokens;
+        }
         return line.split(" ");
     }
 
@@ -23,11 +36,21 @@ public class Cmd {
                 case "echo":
                     System.out.println(echoCommand(tokens));
                     break;
+                case "help":
+                    helpCommand();
+                    break;
                 default:
                     System.out.println("Unrecognized command");
                     break;
             }
         }
+    }
+
+    public void helpCommand() {
+        System.out.println("Available commands:");
+        System.out.println("'exit' - Exits the application");
+        System.out.println("'echo' - Whatever input comes after the command gets printed back");
+        System.out.println();
     }
 
     public void exitCommand() {
--- src/test/java/CmdTest.java
+++ src/test/java/CmdTest.java
@@ -4,7 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class CmdTest {
@@ -25,8 +26,24 @@ public class CmdTest {
     }
 
     @Test
-    public void theCmdShouldSplitAtEachSpaceCharacter() {
+    public void theTokenizerShouldSplitAtEachSpaceCharacter() {
         assertEquals(2, cmd.tokenizeString("cd ..").length);
+    }
+
+    @Test
+    public void theEchoCommandShouldReturnTheCorrectString() {
+        String[] arr = {"echo", "test", "the", "echo", "command"};
+        assertEquals("test the echo command", cmd.echoCommand(arr));
+    }
+
+    @Test @Ignore
+    public void theTokenizerShouldNotSplitBetweenQuotationMarks() {
+        assertEquals(3, cmd.tokenizeString("this 'should have a length of' three").length);
+    }
+
+    @Test @Ignore
+    public void theTokenizerShouldHandleSingleWordQuotes() {
+        assertEquals(6, cmd.tokenizeString("this strings 'length' should be six").length);
     }
 
     @Test
