fb37cecc7d497be657aca2402d42cf825e920d47
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-13 15:32:56 +0200

    Added methods get and set to CurrentDirectory

--- src/main/java/CurrentDirectory.java
+++ src/main/java/CurrentDirectory.java
@@ -1,3 +1,16 @@
 public class CurrentDirectory {
 
+    private static String currentDirectory;
+
+    public CurrentDirectory() {
+        currentDirectory = System.getProperty("user.dir");
+    }
+
+    public String getCurrentDirectory() {
+        return currentDirectory;
+    }
+
+    public void setCurrentDirectory(String path) {
+        currentDirectory = path;
+    }
 }
