c7ce8bca51e828c12a9e7daada8f6d503ca0766d
chrysophylax <chrysophylax@users.noreply.github.com>
2016-10-13 04:51:21 +0200

    Described algorithm for counting words

--- src/main/java/intecmd/commands/WordCount.java
+++ src/main/java/intecmd/commands/WordCount.java
@@ -9,6 +9,7 @@ public class WordCount implements CmdInterface {
 
 	private String separator = ""; //whitespace
 	private File file;
+	private Long count;
 
 	@Override
 	public void in(String[] data) {
@@ -27,7 +28,21 @@ public class WordCount implements CmdInterface {
 
 
 	private void count() {
-		//TODO:
+		//TODO: Implement tokenizer
+
+		/*
+		So, we read in a line using scanner
+		and as long as we have another line
+		->
+		We walk the string as a char array
+		If the last character was a space
+		and the current character is not a space
+		then we increment the counter
+		update last character
+		and take a step forward
+		thus reading a new character
+
+		 */
 	}
 
 	@Override
