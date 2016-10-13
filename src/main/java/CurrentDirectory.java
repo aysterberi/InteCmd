4e2cde9bbf5e490ea8b8207133047b84edbea85f
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-13 15:23:47 +0200

    Added test class CurrentDirectoryTest

--- src/main/java/CurrentDirectory.java
+++ src/main/java/CurrentDirectory.java
@@ -1,5 +1,3 @@
-/**
- * Created by Niklas on 2016-10-13.
- */
 public class CurrentDirectory {
+
 }
---
+++ src/test/java/CurrentDirectoryTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class CurrentDirectoryTest {
+
+    CurrentDirectory cd;
+
+    @Before
+    public void initialize() {
+        cd = new CurrentDirectory();
+    }
+
+}
