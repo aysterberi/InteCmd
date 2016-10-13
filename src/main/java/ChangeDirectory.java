eaaf954dd5bb6f572f2f7b46c8e5ab821e977050
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-13 15:17:40 +0200

    Added method to get home directory

--- src/main/java/ChangeDirectory.java
+++ src/main/java/ChangeDirectory.java
@@ -3,4 +3,8 @@ public class ChangeDirectory {
     public String currentDirectory() {
         return System.getProperty("user.dir");
     }
+
+    public String homeDirectory() {
+        return System.getProperty("user.home");
+    }
 }
--- src/test/java/ChangeDirectoryTest.java
+++ src/test/java/ChangeDirectoryTest.java
@@ -1,9 +1,18 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ChangeDirectoryTest {
 
     private final String USER_DIRECTORY = System.getProperty("user.dir");
+    private final String HOME_DIRECTORY = System.getProperty("user.home");
+
+    private ChangeDirectory cd;
+
+    @Before
+    public void initialize() {
+        cd = new ChangeDirectory();
+    }
 
     @Test
     public void initiateChangeDirectory() {
@@ -12,7 +21,11 @@ public class ChangeDirectoryTest {
 
     @Test
     public void getCurrentDirectory() {
-        ChangeDirectory cd = new ChangeDirectory();
         assertEquals(cd.currentDirectory(), USER_DIRECTORY);
+    }
+
+    @Test
+    public void changeCurrentDirectoryToHomeDirectory() {
+        assertEquals(cd.homeDirectory(), HOME_DIRECTORY);
     }
 }
