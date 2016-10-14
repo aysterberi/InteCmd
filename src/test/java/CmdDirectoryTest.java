c333b34257fed0ed1f8d5e6420e76678a3d12c8b
matt <matt@lotfi.se>
2016-10-14 15:22:25 +0200

    add test for list

--- src/test/java/CmdDirectoryTest.java
+++ src/test/java/CmdDirectoryTest.java
@@ -7,8 +7,10 @@ import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -17,6 +19,7 @@ public class CmdDirectoryTest {
     private LSCommand cmdLS;
     private String defaultPath;
     private File mockedFile;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
@@ -24,6 +27,7 @@ public class CmdDirectoryTest {
     @Before
     public void setUpTests() {
         defaultPath = "/Program Files/";
+        System.setOut(new PrintStream(outContent));
 
 //        File mockedFile = Mockito.mock(File.class);
 //        Mockito.when(mockedFile.exists()).thenReturn(true);
@@ -65,7 +69,7 @@ public class CmdDirectoryTest {
         final File file2 = temporaryFolder.newFile("file2.txt");
         final File file3 = temporaryFolder.newFile("file3.txt");
         cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
-        assertEquals(cmdLS.getFiles().size(), 3);
+        assertEquals(3,cmdLS.getFiles().size());
     }
 
     @Test
@@ -74,7 +78,7 @@ public class CmdDirectoryTest {
         final File dir2 = temporaryFolder.newFolder("directory2");
         final File dir3 = temporaryFolder.newFolder("directory3");
         cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
-        assertEquals(cmdLS.getDirectories().size(), 3);
+        assertEquals(3,cmdLS.getDirectories().size());
     }
 
     @Test
@@ -86,6 +90,20 @@ public class CmdDirectoryTest {
         final File dir2 = temporaryFolder.newFolder("directory2");
         final File dir3 = temporaryFolder.newFolder("directory3");
         cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
-        assertEquals(cmdLS.getDirectories().size() + cmdLS.getFiles().size(), 6);
+        assertEquals(6,cmdLS.getDirectories().size() + cmdLS.getFiles().size());
+    }
+
+    @Test
+    public void theLFlagShouldListAllContent () throws IOException {
+        String command = "ls -l";
+        final File file1 = temporaryFolder.newFile("file1.txt");
+        final File file2 = temporaryFolder.newFile("file2.txt");
+        final File file3 = temporaryFolder.newFile("file3.txt");
+        final File dir1 = temporaryFolder.newFolder("directory1");
+        final File dir2 = temporaryFolder.newFolder("directory2");
+        final File dir3 = temporaryFolder.newFolder("directory3");
+        cmdLS = new LSCommand(command.split(" ") , temporaryFolder.getRoot().getPath());
+        assertEquals("Directories\ndirectory1\ndirectory2\ndirectory3" +
+                "\nFiles\nfile1.txt\nfile2.txt\nfile3.txt", outContent.toString());
     }
 }
