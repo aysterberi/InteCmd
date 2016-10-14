7f28dcf2fce9e6d114a05f527f60b6f32965f855
matt <matt@lotfi.se>
2016-10-14 14:10:42 +0200

    add FindAll directories test

--- src/test/java/CmdDirectoryTest.java
+++ src/test/java/CmdDirectoryTest.java
@@ -65,8 +65,16 @@ public class CmdDirectoryTest {
         final File file1 = temporaryFolder.newFile("file1.txt");
         final File file2 = temporaryFolder.newFile("file2.txt");
         final File file3 = temporaryFolder.newFile("file3.txt");
-        final File directory1 = temporaryFolder.newFolder("ge");
         cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
         assertEquals(cmdLS.getFiles().size(), 3);
+    }
+
+    @Test
+    public void theDirectoryListShouldFindAllDirectories () throws IOException {
+        final File dir1 = temporaryFolder.newFolder("directory1");
+        final File dir2 = temporaryFolder.newFolder("directory2");
+        final File dir3 = temporaryFolder.newFolder("directory3");
+        cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
+        assertEquals(cmdLS.getDirectories().size(), 3);
     }
 }
