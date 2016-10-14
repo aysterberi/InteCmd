7b8848d61bca853e8ff50e1eacde96f07e605d24
matt <matt@lotfi.se>
2016-10-14 14:15:28 +0200

    add test for both files & directory

--- src/test/java/CmdDirectoryTest.java
+++ src/test/java/CmdDirectoryTest.java
@@ -77,4 +77,16 @@ public class CmdDirectoryTest {
         cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
         assertEquals(cmdLS.getDirectories().size(), 3);
     }
+    @Test
+    public void theFileAndDirectoryListShouldFindAll () throws IOException {
+        final File file1 = temporaryFolder.newFile("file1.txt");
+        final File file2 = temporaryFolder.newFile("file2.txt");
+        final File file3 = temporaryFolder.newFile("file3.txt");
+        final File dir1 = temporaryFolder.newFolder("directory1");
+        final File dir2 = temporaryFolder.newFolder("directory2");
+        final File dir3 = temporaryFolder.newFolder("directory3");
+        cmdLS = new LSCommand(temporaryFolder.getRoot().getPath());
+        assertEquals(cmdLS.getDirectories().size() + cmdLS.getFiles().size(), 6);
+    }
+
 }
