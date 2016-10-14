9369d9af44fbcd00828806e00bb1397cf0b873ef
Mattin Lotfi <matt@lotfi.se>
2016-10-14 19:13:58 +0200

    Refactor name of class

--- src/test/java/LSTest.java
+++ src/test/java/LSTest.java
@@ -10,7 +10,7 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
-public class CmdDirectoryTest {
+public class LSTest {
     private LSCommand cmdLS;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
