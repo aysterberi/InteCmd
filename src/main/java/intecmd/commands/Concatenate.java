ff0a54c5c8276d0a35006fb6a25d86c102303bcb
Billy <chrysophylax@users.noreply.github.com>
2016-10-17 14:06:28 +0200

    Wrote basic cat functionality

--- src/main/java/intecmd/commands/Concatenate.java
+++ src/main/java/intecmd/commands/Concatenate.java
@@ -15,16 +15,52 @@ package intecmd.commands;
 
 import intecmd.CommandInterface;
 
-import java.io.InputStream;
+import java.io.*;
 
 public class Concatenate implements CommandInterface {
+	private boolean isBuffered = true;
 	@Override
 	public void in(String[] data) {
+
+		for(String s : data) {
+			if (s.equals("-u"))
+			{
+				isBuffered = false;
+				continue;
+			} else {
+				try {
+					FileInputStream fis = new FileInputStream(s);
+					cat(fis, s);
+					fis.close();
+				} catch (java.io.IOException e) {
+					System.out.println("Could not open " + s);
+					return;
+				}
+			}
+		}
 
 	}
 
 	public void cat(InputStream is, String fileName) {
 
+		byte[] buf = new byte[8192]; //8KB
+		int r;
+		try {
+		if(isBuffered){
+			while(is.read(buf) != -1){
+				try {
+					System.out.write(buf);
+				} catch (IOException e) {
+					System.out.println("Error writing " + fileName);
+				}
+			}
+		} else if (!isBuffered) {
+			while((r = is.read()) != -1) {
+				System.out.write(r);
+			}
+		}  } catch (IOException e) {
+			System.out.println("Error reading from " + fileName);
+		}
 	}
 	@Override
 	public String out() {
