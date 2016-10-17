4f8c92d41004484a976809ff898a618bb053fad5
Joakim Berglund <joakimberglund@live.se>
2016-10-17 14:01:22 +0200

    add grep class

---
+++ src/main/java/GrepCommand.java
@@ -0,0 +1,59 @@
+import java.io.*;
+
+public class GrepCommand implements CmdInterface {
+
+    String[] tokens;
+    private CurrentDirectory currentDirectory = new CurrentDirectory();
+
+    public GrepCommand(String[] tokens) {
+        this.tokens = tokens;
+        if (tokens.length > 3)
+            tooManyArguments();
+        if (tokens[1].equals("help"))
+            help();
+        else
+            executeSearch(tokens);
+    }
+
+    public String tooManyArguments() {
+        return "Too many arguments, try again";
+    }
+
+    public String executeSearch(String[] tokens) {
+        File[] files = fileFinder(currentDirectory.toString());
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader;
+
+        if (files.length == 0)
+            return "No files with that name";
+
+        for (File f : files) {
+            try {
+                bufferedReader = new BufferedReader(new FileReader(f.getName()));
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    if (line.contains(tokens[1])) {
+                        stringBuilder.append(line);
+                    }
+                    line = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+        if(stringBuilder.toString().isEmpty())
+            return "error message";
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String help() {
+        return "test help";
+    }
+
+    public File[] fileFinder(String directory) {
+        File dir = new File(directory);
+        return dir.listFiles((dir1, name) -> name.equals(this.tokens[2]));
+    }
+}
