96954e0cec5bc9ded6e979e3317a24d4c18b02c5
Billy <chrysophylax@users.noreply.github.com>
2016-10-17 12:42:54 +0200

    Starting on cat command

---
+++ src/main/java/intecmd/commands/Concatenate.java
@@ -0,0 +1,20 @@
+package intecmd.commands;
+
+import intecmd.CommandInterface;
+
+public class Concatenate implements CommandInterface {
+	@Override
+	public void in(String[] data) {
+
+	}
+
+	@Override
+	public String out() {
+		return null;
+	}
+
+	@Override
+	public String help() {
+		return null;
+	}
+}
---
+++ src/test/java/ConcatenateTest.java
@@ -0,0 +1,7 @@
+import intecmd.commands.Concatenate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConcatenateTest {
+}
