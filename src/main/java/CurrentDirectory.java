5631e6c3ed02de6b9ed7ed88493f527d094b2428
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-18 12:01:29 +0200

    Refactored construct of CurrentDirectory

--- src/main/java/CurrentDirectory.java
+++ src/main/java/CurrentDirectory.java
@@ -6,15 +6,19 @@ public class CurrentDirectory {
     private static String currentDirectory = System.getProperty("user.dir");
 
     public void setCurrentDirectory(String path) {
+        verifyPath(path);
+        if(!pathExists(path))
+            throw new IllegalArgumentException("Path does not exist.");
+        currentDirectory = path;
+    }
+
+    private void verifyPath(String path) {
         if(path == null)
             throw new IllegalArgumentException("Path was null.");
         else if(path.equals(SEPARATOR))
             throw new IllegalArgumentException("Path can't exceed root directory.");
         else if (path.equals(""))
             throw new IllegalArgumentException("Path can't be empty.");
-        if(!pathExists(path))
-            throw new IllegalArgumentException("Path does not exist.");
-        currentDirectory = path;
     }
 
     private boolean pathExists(String path) {
