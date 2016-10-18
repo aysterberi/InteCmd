c4a3154e49ed6e294bcdc6e173129d54f11695e4
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-18 17:30:59 +0200

    verifyPath() now considers which OS the user uses.

--- src/main/java/CurrentDirectory.java
+++ src/main/java/CurrentDirectory.java
@@ -1,8 +1,11 @@
+import com.sun.deploy.util.SystemUtils;
+
 import java.io.File;
 
 public class CurrentDirectory {
 
-    public static final String SEPARATOR = "\\";
+    public static final String SEPARATOR = System.getProperty("file.separator");
+    public static final String USER_SYSTEM = System.getProperty("os.name");
     private static String currentDirectory = System.getProperty("user.dir");
 
     public void setCurrentDirectory(String path) {
@@ -15,7 +18,7 @@ public class CurrentDirectory {
     private void verifyPath(String path) {
         if(path == null)
             throw new IllegalArgumentException("Path was null.");
-        else if(path.equals(SEPARATOR))
+        else if(USER_SYSTEM.startsWith("Windows") && path.equals(SEPARATOR))
             throw new IllegalArgumentException("Path can't exceed root directory.");
         else if (path.equals(""))
             throw new IllegalArgumentException("Path can't be empty.");
