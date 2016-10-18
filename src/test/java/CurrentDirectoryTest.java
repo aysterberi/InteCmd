1f9948e928c352a7837a6f7b947537f8fb6c8a1b
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-18 11:26:35 +0200

    Added test class for CurrentDirectory, added initialization method.

---
+++ src/test/java/CurrentDirectoryTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CurrentDirectoryTest {
+
+    CurrentDirectory currentDirectory;
+
+    @Before
+    public void initialize() {
+        currentDirectory = new CurrentDirectory();
+    }
+}
