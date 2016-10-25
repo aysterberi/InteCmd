2251216844389478c5c3a3fb274ab6b7f5436cff
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-25 06:22:47 +0200

    Added toString()

--- src/test/java/MockDirectory.java
+++ src/test/java/MockDirectory.java
@@ -6,6 +6,7 @@ public class MockDirectory {
     protected String name;
 
     public MockDirectory(String name) {
+        System.out.println("New directory name: " + name);
         if(directoryNameIsValid(name))
             this.name = name;
         else
@@ -14,5 +15,9 @@ public class MockDirectory {
 
     private boolean directoryNameIsValid(String name) {
         return System.getProperty("os.name").startsWith("Windows") ? name.matches(ChangeDirectory.PATTERN_WINDOWS_DIRECTORY) : name.matches(ChangeDirectory.PATTERN_UNIX_DIRECTORY);
+    }
+
+    public String toString() {
+        return name;
     }
 }
