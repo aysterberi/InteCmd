96594b87208177fb041930767fbcd3f75b05c4ff
Niklas Åkerlund <akerlund.niklas@gmail.com>
2016-10-25 17:27:44 +0200

    Removed validation of MockDirectory name.

--- src/test/java/MockDirectory.java
+++ src/test/java/MockDirectory.java
@@ -6,14 +6,7 @@ public class MockDirectory {
     protected String name;
 
     public MockDirectory(String name) {
-        if(directoryNameIsValid(name))
-            this.name = name;
-        else
-            throw new IllegalArgumentException("Directory name is invalid.");
-    }
-
-    private boolean directoryNameIsValid(String name) {
-        return System.getProperty("os.name").startsWith("Windows") ? name.matches(ChangeDirectory.PATTERN_WINDOWS_DIRECTORY) : name.matches(ChangeDirectory.PATTERN_UNIX_DIRECTORY);
+        this.name = name;
     }
 
     public String toString() {
